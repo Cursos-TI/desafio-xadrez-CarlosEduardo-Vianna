4fe3895d6bbb3b48039e16c521d3ba93557f9b39
Maverick <caducastro14@gmail.com>
2025-10-28 21:35:03 +0000

    Adicionei a recursividade e loops complexos

--- xadrez.c
+++ xadrez.c
@@ -1,52 +1,61 @@
 #include <stdio.h>
 
-int main() {
-
-    int b = 0;
-    int r = 0;
-    int c = 0;
-    int opcao;
-
-    printf("### Movimento da torre ###\n");
-
-    // mover a torre 5 casas a direita
-    for (int t = 0; t < 5; t++){
-        printf("Direita\n"); // imprime a direcao do movimento
+void  moverTorre(int casas) {
+    // move a torre 5 casas a direita
+    if (casas > 0) {
+        printf("Direira \n");
+        moverTorre(casas - 1);
     }
+}
 
-
-    printf("\n### Movimento do bispo ###\n");
-
-    // mover o bispo em 5 casas a diagonal 
-    while (b < 5)
-    {
-        printf("Cima\n");
+void moverBispo(int casas) {   
+    // move o bispo 5 casas a diagonal
+    // loop externo
+    for (int i = casas; i > 0; i--) {
+        // loop interno
+        for (int j = 1; j > 0; j--) {
+            printf("Cima\n");
+        }
         printf("Direita\n");
-        b++;
     }
+}
 
-    printf("\n### Movimento da rainha ###\n");
-
-    //mover a rainha 8 casas a esquerda
+void moverRainha(int casas) {
+    // move a rainha 8 casas a esquerda
     do
     {
         printf("esquerda\n");
-        r++;
-    } while (r < 8);
+        casas--;
+    } while (casas > 0);
+}
+
+int main() {
+
+    int opcao;
+    int c = 0;
+
+    printf("\n### Movimento da Torre ###\n");
+    moverTorre(5);
+
+    printf("\n### Movimento do Bispo ###\n");
+    moverBispo(5);
+
+    printf("\n### Movimento da Rainha ###\n");
+    moverRainha(8);
 
     printf("\n### Movimento do cavalo ###\n");
 
-    // O usuario escolhe a direção que o cavalo vai se mover
+    // O usuario escolhe em qual direcao o cavalo vai se mover
     printf("Escolha a direção que o cavalo vai se mover!\n");
     printf("1. baixo e esquerda\n");
     printf("2. baixo e direita\n");
+    printf("3. cima e direita\n");
     scanf("%d", &opcao);
 
-    // menu interativo
     switch (opcao)
     {
-    case 1:
-         while (c < 1)
+    case 1: // Movimento Baixo e Esquerda
+        while (c < 1)
     {
         c++;
 
@@ -59,8 +68,8 @@ int main() {
     }
         break;
     
-    case 2:
-         while (c < 1)
+    case 2: // Movimento Baixo e Direira
+        while (c < 1)
     {
         c++;
 
@@ -73,6 +82,29 @@ int main() {
     }
         break;
     
+    
+    case 3: // Movimento Cima e Direita
+        for (int i = 0, j = 0; i < 2 || j < 1; ) // múltiplas variáveis no loop
+    {
+        if (i < 2) {
+            printf("Cima\n");
+            i++;
+            continue; // volta ao início até terminar o movimento vertical
+        }
+
+        if (j < 1) {
+            printf("Direita\n");
+            j++;
+        }
+
+        // se ambos movimentos foram feitos, interrompe o loop
+        if (i >= 2 && j >= 1) {
+            break;
+        }
+    }
+        break;
+
+
     default:
         printf("Opção invalida");
     
@@ -81,3 +113,4 @@ int main() {
 
     return 0;
 }
+
